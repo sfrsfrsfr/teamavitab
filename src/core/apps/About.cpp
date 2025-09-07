3cb7f338b6aabbcabf77e2678ed27d423dfd73e9
Stefan Frank <sfrsfr@gmail.com>
2025-09-07 13:49:16 +0200

    use the Window widgets 'add' method

--- src/core/apps/About.cpp
+++ src/core/apps/About.cpp
@@ -25,6 +25,7 @@ About::About(FuncsPtr appFuncs):
     window(std::make_shared<Window>(getUIContainer(), "About AviTab")),
     label(std::make_shared<Label>(window, ""))
 {
+    window->add(label);
     window->setOnClose([this] () { exit(); });
 
     std::string aboutText =
