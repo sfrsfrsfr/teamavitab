54fcba06829f3ec78acb0df67c1fc98a9cbc37ca
Stefan Frank <sfrsfr@gmail.com>
2026-08-22 13:51:00 +0200

    Revert "disable Tabview sliding"
    This reverts commit 71b18833a9da819726d89c870d0de6e3198ce593.

--- src/core/gui/widgets/TabGroup.cpp
+++ src/core/gui/widgets/TabGroup.cpp
@@ -27,8 +27,6 @@ TabGroup::TabGroup(WidgetPtr parent):
 {
     // FIXME tab_height/tab_width
     lv_obj_t *tabs = lv_tabview_create(parentObj(), LV_DIR_TOP, 20);
-    // disable Tabview sliding
-    lv_obj_clear_flag(lv_tabview_get_content(tabs), LV_OBJ_FLAG_SCROLLABLE);
 
     setObj(tabs);
 }
