3ce5b192888f9c0bf01e1998ad65003a72c32bdc
Stefan Frank <sfrsfr@gmail.com>
2026-08-30 17:21:50 +0200

    update lv_conf.h settings

--- extern/lvgl/patches/lv_conf.h
+++ extern/lvgl/patches/lv_conf.h
@@ -497,7 +497,7 @@
  *  If size is not set to 0, the decoder will fail to decode when the cache is full.
  *  If size is 0, the cache function is not enabled and the decoded memory will be
  *  released immediately after use. */
-#define LV_CACHE_DEF_SIZE       4096
+#define LV_CACHE_DEF_SIZE       4096 * 4024
 
 /** Default number of image header cache entries. The cache is used to store the headers of images
  *  The main logic is like `LV_CACHE_DEF_SIZE` but for image headers. */
@@ -505,7 +505,7 @@
 
 /** Number of stops allowed per gradient. Increase this to allow more stops.
  *  This adds (sizeof(lv_color_t) + 1) bytes per additional stop. */
-#define LV_GRADIENT_MAX_STOPS   2
+#define LV_GRADIENT_MAX_STOPS   8
 
 /** Adjust color mix functions rounding. GPUs might calculate color mix (blending) differently.
  *  - 0:   round down,
